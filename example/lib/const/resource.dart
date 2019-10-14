b5b557fc5e929b7f5b8ca8e79317be1e6bb747a1
Caijinglong <cjl_spy@163.com>
2019-10-14 15:57:02 +0800

    Fix ios orientation.

--- example/lib/const/resource.dart
+++ example/lib/const/resource.dart
@@ -1,6 +1,9 @@
 /// generate by resouce_generator library, shouldn't edit.
 class R {
     
+  /// ![preview](file:///Users/cai/Documents/GitHub/flutter_image_editor/example/./assets/have-exif-8.jpg)
+  static const String ASSETS_HAVE_EXIF_8_JPG = "assets/have-exif-8.jpg";
+    
   /// ![preview](file:///Users/cai/Documents/GitHub/flutter_image_editor/example/./assets/icon.png)
   static const String ASSETS_ICON_PNG = "assets/icon.png";
 }
--- ios/Classes/UIImageHandler.swift
+++ ios/Classes/UIImageHandler.swift
@@ -6,43 +6,66 @@ import Foundation
 import UIKit
 
 class UIImageHandler {
+    var image: UIImage
 
-  var image: UIImage
-
-  init(image:UIImage) {
-    self.image = image
-  }
-
-  func handleImage(options: [FlutterImageEditorOption]) {
-    for option in options {
-      if option is FlipOption {
-        image = handleFlip(option as! FlipOption)
-      } else if option is ClipOption {
-        image = handleClip(option as! ClipOption)
-      } else if option is RotateOption {
-        image = handleRotate(option as! RotateOption)
-      }
+    init(image: UIImage) {
+        self.image = image
     }
-  }
 
-    func outputFile(targetPath:String) {
+    func handleImage(options: [FlutterImageEditorOption], fixOrientation: Bool = true) {
+        if fixOrientation {
+            let orientation = image.imageOrientation
+            switch orientation {
+            case .up:
+                break
+            case .down:
+                image = image.rotate(180)
+            case .left:
+                image = image.rotate(90)
+            case .right:
+                image = image.rotate(270)
+            case .upMirrored:
+                image = image.flip(horizontal: true, vertical: false)
+            case .downMirrored:
+                image = image.flip(horizontal: false, vertical: true)
+            case .leftMirrored:
+                image = image.rotate(90)
+                image = image.flip(horizontal: true, vertical: false)
+            case .rightMirrored:
+                image = image.rotate(270)
+                image = image.flip(horizontal: true, vertical: false)
+            }
+        }
+
+        for option in options {
+            if option is FlipOption {
+                image = handleFlip(option as! FlipOption)
+            } else if option is ClipOption {
+                image = handleClip(option as! ClipOption)
+            } else if option is RotateOption {
+                image = handleRotate(option as! RotateOption)
+            }
+        }
+    }
+
+    func outputFile(targetPath: String) {
         let data = UIImagePNGRepresentation(image)
         try! data?.write(to: URL(fileURLWithPath: targetPath))
     }
-    
-    func outputMemory()->Data?{
+
+    func outputMemory() -> Data? {
         return UIImagePNGRepresentation(image)
     }
 
-  private func handleRotate(_ option: RotateOption) -> UIImage {
-    return image.rotate(option.degree)
-  }
+    private func handleRotate(_ option: RotateOption) -> UIImage {
+        return image.rotate(option.degree)
+    }
 
-  private func handleClip(_ option: ClipOption) -> UIImage {
-    return image.crop(x: option.x, y: option.y, width: option.width, height: option.height)
-  }
+    private func handleClip(_ option: ClipOption) -> UIImage {
+        return image.crop(x: option.x, y: option.y, width: option.width, height: option.height)
+    }
 
-  private func handleFlip(_ option: FlipOption) -> UIImage {
-    return image.flip(horizontal: option.horizontal, vertical: option.vertical)
-  }
+    private func handleFlip(_ option: FlipOption) -> UIImage {
+        return image.flip(horizontal: option.horizontal, vertical: option.vertical)
+    }
 }
