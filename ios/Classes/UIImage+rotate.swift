d453251cabfa14de786dcca0f55ab878f560ba01
Caijinglong <cjl_spy@163.com>
2019-10-25 15:45:46 +0800

    Fix error in rotate 180 degree. Fix clip number.

--- ios/Classes/UIImage+rotate.swift
+++ ios/Classes/UIImage+rotate.swift
@@ -9,34 +9,36 @@ import Foundation
 
 extension UIImage {
     func rotate(_ angle: Int) -> UIImage {
-        // 转为弧度制
+//        // 转为弧度制
         let radian = CGFloat(angle) * CGFloat.pi / 180
-        
-        // 计算出旋转后需要的尺寸, 因为非180度的倍数会发生宽高变化, 非90的整数倍会使用白色填充背景色
-        let rotatedSize = CGRect(origin: .zero, size: size)
-            .applying(CGAffineTransform(rotationAngle: CGFloat(radian)))
-            .integral.size
+        return rotate(radians: radian)
+    }
 
-        // 使用CG开始绘制
-        UIGraphicsBeginImageContext(rotatedSize)
-        guard let ctx = UIGraphicsGetCurrentContext() else {
+    func rotate(radians: CGFloat) -> UIImage {
+        var newSize = CGRect(origin: CGPoint.zero, size: size).applying(CGAffineTransform(rotationAngle: radians)).size
+        // Trim off the extremely small float value to prevent core graphics from rounding it up
+        newSize.width = floor(newSize.width)
+        newSize.height = floor(newSize.height)
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, scale)
+        guard let context = UIGraphicsGetCurrentContext() else {
             return self
         }
 
-        let origin = CGPoint(x: rotatedSize.width / 2.0,
-                             y: rotatedSize.height / 2.0)
+        // Move origin to middle
+        context.translateBy(x: newSize.width / 2, y: newSize.height / 2)
+        // Rotate around middle
+        context.rotate(by: radians)
+        // Draw the image at its center
+        draw(in: CGRect(x: -size.width / 2, y: -size.height / 2, width: size.width, height: size.height))
 
-        // 开始绘制
-        ctx.translateBy(x: origin.x, y: origin.y)
-        ctx.rotate(by: radian)
-        draw(in: CGRect(x: -origin.y, y: -origin.x, width: size.width, height: size.height))
-
-        guard let rotatedImage = UIGraphicsGetImageFromCurrentImageContext() else {
+        guard let newImage = UIGraphicsGetImageFromCurrentImageContext() else {
             UIGraphicsEndImageContext()
             return self
         }
+
         UIGraphicsEndImageContext()
 
-        return rotatedImage
+        return newImage
     }
 }
