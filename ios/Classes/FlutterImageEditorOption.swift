7f412f02cde40df9cb092d030bfd69abf47f6bbc
Caijinglong <cjl_spy@163.com>
2020-05-22 16:34:59 +0800

    Delete swift file , and move objc file to classes.

--- ios/Classes/FlutterImageEditorOption.swift
+++
@@ -1,7 +0,0 @@
-//
-// Created by Caijinglong on 2019-10-09.
-//
-
-import Foundation
-
-protocol FlutterImageEditorOption {}
--- ios/Classes/FlutterImageEditorPlugin.m
+++ ios/Classes/FlutterImageEditorPlugin.m
@@ -1,10 +1,8 @@
 #import "FlutterImageEditorPlugin.h"
-#import <image_editor/image_editor-Swift.h>
-#import "oc/FIEPlugin.h"
+#import "FIEPlugin.h"
 
 @implementation FlutterImageEditorPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  //  [SwiftFlutterImageEditorPlugin registerWithRegistrar:registrar];
   [FIEPlugin registerWithRegistrar:registrar];
 }
 
