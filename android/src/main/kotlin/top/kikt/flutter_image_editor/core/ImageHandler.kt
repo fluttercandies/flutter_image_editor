f42c7608664f76812fa5b77435a75eb917e347a5
Caijinglong <cjl_spy@163.com>
2020-05-27 17:34:59 +0800

    Merge image

--- android/src/main/kotlin/top/kikt/flutter_image_editor/core/ImageHandler.kt
+++ android/src/main/kotlin/top/kikt/flutter_image_editor/core/ImageHandler.kt
@@ -15,7 +15,7 @@ import kotlin.math.min
 /// create 2019-10-08 by cai
 
 class ImageHandler(private var bitmap: Bitmap) {
-
+  
   fun handle(options: List<Option>) {
     for (option in options) {
       when (option) {
@@ -29,39 +29,35 @@ class ImageHandler(private var bitmap: Bitmap) {
       }
     }
   }
-
+  
   private fun handleMixImage(option: MixImageOpt): Bitmap {
     val newBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, bitmap.config)
     val canvas = Canvas(newBitmap)
     canvas.drawBitmap(bitmap, 0F, 0F, null)
-
+    
     val src = BitmapFactory.decodeByteArray(option.img, 0, option.img.count())
-
+    
     val paint = Paint()
     paint.xfermode = PorterDuffXfermode(option.porterDuffMode)
-
-//    val srcRect = Rect(option.x, option.y, option.x + option.w, option.y + option.h)
-//    val dstRect = Rect(0, 0, bitmap.width, bitmap.height)
-
+    
     val dstRect = Rect(option.x, option.y, option.x + option.w, option.y + option.h)
-    val srcRect = Rect(0, 0, bitmap.width, bitmap.height)
-    canvas.drawBitmap(src, srcRect, dstRect, paint)
-
+    canvas.drawBitmap(src, null, dstRect, paint)
+    
     return newBitmap
   }
-
+  
   private fun handleScale(option: ScaleOption): Bitmap {
     val w = min(bitmap.width, option.width)
     val h = min(bitmap.height, option.height)
-
+    
     val newBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-
+    
     val canvas = Canvas(newBitmap)
-
+    
     val p = Paint()
-
+    
     val m = Matrix()
-
+    
     val width: Int = bitmap.width
     val height: Int = bitmap.height
     if (width != w || height != h) {
@@ -69,12 +65,12 @@ class ImageHandler(private var bitmap: Bitmap) {
       val sy: Float = h / height.toFloat()
       m.setScale(sx, sy)
     }
-
+    
     canvas.drawBitmap(bitmap, m, p)
-
+    
     return newBitmap
   }
-
+  
   private fun handleRotate(option: RotateOption): Bitmap {
     val tmpBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
     val canvas = Canvas(tmpBitmap)
@@ -82,12 +78,12 @@ class ImageHandler(private var bitmap: Bitmap) {
       //      val rotate = option.angle.toFloat() / 180 * Math.PI
       this.postRotate(option.angle.toFloat())
     }
-
+    
     val out = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
     canvas.drawBitmap(out, matrix, null)
     return out
   }
-
+  
   private fun handleFlip(option: FlipOption): Bitmap {
     val tmpBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
     val canvas = Canvas(tmpBitmap)
@@ -96,81 +92,81 @@ class ImageHandler(private var bitmap: Bitmap) {
       val y = if (option.vertical) -1F else 1F
       postScale(x, y)
     }
-
+    
     val out = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
     canvas.drawBitmap(out, matrix, null)
     return out
   }
-
+  
   private fun handleClip(option: ClipOption): Bitmap {
     val x = option.x
     val y = option.y
     return Bitmap.createBitmap(bitmap, x, y, option.width, option.height, null, false)
   }
-
+  
   private fun handleColor(option: ColorOption): Bitmap {
     val newBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, bitmap.config)
-
+    
     val canvas = Canvas(newBitmap)
-
+    
     val paint = Paint()
     paint.colorFilter = ColorMatrixColorFilter(option.matrix)
-
+    
     canvas.drawBitmap(bitmap, 0F, 0F, paint)
-
+    
     return newBitmap
   }
-
+  
   private fun handleText(option: AddTextOpt): Bitmap {
     val newBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, bitmap.config)
-
+    
     val canvas = Canvas(newBitmap)
     val paint = Paint()
-
+    
     canvas.drawBitmap(bitmap, 0F, 0F, paint)
-
+    
     for (text in option.texts) {
       drawText(text, canvas)
     }
-
+    
     return newBitmap
   }
-
+  
   private fun drawText(text: Text, canvas: Canvas) {
     val textPaint = TextPaint(Paint.ANTI_ALIAS_FLAG)
     textPaint.color = Color.argb(text.a, text.r, text.g, text.b)
     textPaint.textSize = text.fontSizePx.toFloat()
 //    canvas.drawText(text.text, text.x.toFloat(), text.y.toFloat(), textPaint)
-
+    
     val staticLayout = getStaticLayout(text, textPaint, canvas.width - text.x)
-
+    
     canvas.translate(text.x.toFloat(), text.y.toFloat())
     staticLayout.draw(canvas)
     canvas.translate((-text.x).toFloat(), (-text.y).toFloat())
   }
-
+  
   @Suppress("DEPRECATION")
   private fun getStaticLayout(text: Text, textPaint: TextPaint, width: Int): StaticLayout {
     return if (Build.VERSION.SDK_INT >= 23) {
       StaticLayout.Builder.obtain(
-              text.text, 0, text.text.length, textPaint, width
+        text.text, 0, text.text.length, textPaint, width
       ).build()
     } else {
       StaticLayout(text.text, textPaint, width, Layout.Alignment.ALIGN_NORMAL, 1.0F, 0.0F, true)
     }
   }
-
+  
   fun outputToFile(dstPath: String, formatOption: FormatOption) {
     val outputStream = FileOutputStream(dstPath)
     output(outputStream, formatOption)
   }
-
+  
   fun outputByteArray(formatOption: FormatOption): ByteArray {
     val outputStream = ByteArrayOutputStream()
     output(outputStream, formatOption)
     return outputStream.toByteArray()
   }
-
+  
   private fun output(outputStream: OutputStream, formatOption: FormatOption) {
     outputStream.use {
       if (formatOption.format == 0) {
